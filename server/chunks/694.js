8604f0bf3af24f8bfd0a5d56f09afd4a940e5021
Morgan Mastrangelo <mmastrangelo1120@gmail.com>
2023-06-19 18:39:26 -0400

    completed initial version

---
+++ server/chunks/694.js
@@ -0,0 +1,123 @@
+"use strict";
+exports.id = 694;
+exports.ids = [694];
+exports.modules = {
+
+/***/ 5694:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   Z: () => (/* binding */ ModeNavbar)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5893);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5675);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _mui_icons_material_Twitter__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5631);
+/* harmony import */ var _mui_icons_material_Twitter__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_mui_icons_material_Twitter__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _mui_icons_material_SportsEsports__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(7928);
+/* harmony import */ var _mui_icons_material_SportsEsports__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_mui_icons_material_SportsEsports__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(1664);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_4__);
+
+
+
+
+
+function ModeNavbar(props) {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: "flex flex-col md:flex-row w-full h-28 pl-4 pr-4 opacity-0 animate-fade-down animate-duration-50",
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: "flex items-center w-full m-4",
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_4___default()), {
+                        href: "/",
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_1___default()), {
+                            alt: "logo",
+                            src: "/assets/svg/vector.svg",
+                            width: 40,
+                            height: 45,
+                            className: "pr-4",
+                            priority: true
+                        })
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: "flex relative w-full h-16",
+                        style: {
+                            background: "rgba(255,255,255,0.1)"
+                        },
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                style: {
+                                    background: "#00FFE6",
+                                    width: `${props.progress}%`,
+                                    animation: "progress-anim 1s"
+                                }
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                className: "absolute top-0 left-1/2 w-0.5 h-full bg-gray-800"
+                            })
+                        ]
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: "flex items-center w-full",
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: "flex justify-center items-center relative w-4/12 h-16",
+                        children: [
+                            "ABOUT",
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                className: "absolute bottom-0 left-0 text-gray-500 text-sm",
+                                children: "001"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: "flex justify-center items-center relative w-2/12 h-16",
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((_mui_icons_material_SportsEsports__WEBPACK_IMPORTED_MODULE_3___default()), {}),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                className: "absolute bottom-0 left-0 text-gray-500 text-sm",
+                                children: "002"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: "flex justify-center items-center relative w-2/12 h-16",
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((_mui_icons_material_Twitter__WEBPACK_IMPORTED_MODULE_2___default()), {}),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                className: "absolute bottom-0 left-0 text-gray-500 text-sm",
+                                children: "003"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: "flex justify-center items-center relative w-4/12 h-16",
+                        children: [
+                            "LAUNCH ULTIVERSE",
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                className: "absolute bottom-0 left-0 text-gray-500 text-sm",
+                                children: "004"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                        className: "flex flex-col justify-center items-center w-2/12 h-16",
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                            className: "w-12 h-0.5 m-5 bg-gray-500"
+                        })
+                    })
+                ]
+            })
+        ]
+    });
+}
+
+
+/***/ })
+
+};
+;
